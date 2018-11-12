27c96ec6c474d6ca02004dda1f793fdf49fe9805
wan-zhen <kelly811124811124@hotmail.com>
2018-11-12 11:04:06 +0800

    補上 mutations 說明

--- src/components/HelloWorld_store.js
+++ src/components/HelloWorld_store.js
@@ -2,6 +2,9 @@ export default {
   state: {
     show: false
   },
+  // mutations 中的方法是不分组件的，其他地方也有一樣的 mutations -> toggleShow
+  // 執行提交會執行所有的 toggleShow 方法
+  // mutations里的操作必须是同步的
   mutations: {
     toggleShow(state) {
       state.show = !state.show;
