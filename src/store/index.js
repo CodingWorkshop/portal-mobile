2f2fa3a5d7e40bb3c1f3e327aa3a9d82fed1a03b
cyan92128505 <cyan92128505@gmail.com>
2018-11-17 14:47:54 +0800

    apply package vuex-persistedstate

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import createPersistedState from 'vuex-persistedstate';
 
 import show from './modules/show';
 import login from './modules/login';
@@ -9,5 +10,18 @@ export default new Vuex.Store({
   modules: {
     show: show,
     login: login
-  }
+  },
+  plugins: [
+    createPersistedState({
+      storage: {
+        getItem: key => localStorage.getItem(key),
+        setItem: (key, value) =>
+          localStorage.setItem(key, value, {
+            expires: 3,
+            secure: true
+          }),
+        removeItem: key => localStorage.removeItem(key)
+      }
+    })
+  ]
 });
