0a33bd69105237954353a0110184e70dc54fb4ad
wan-zhen <kelly811124811124@hotmail.com>
2018-11-20 11:41:41 +0800

    把 fontAwesome 從 main 中拉出去

--- src/main.js
+++ src/main.js
@@ -10,29 +10,7 @@ Vue.config.productionTip = false;
 Vue.use(VueAxios, axios);
 
 import './plugins/iview.js';
-
-//FontAwesome
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { faGamepad } from '@fortawesome/free-solid-svg-icons';
-import { faMoneyBillAlt } from '@fortawesome/free-solid-svg-icons';
-import { faPhoneVolume } from '@fortawesome/free-solid-svg-icons';
-import { faGift } from '@fortawesome/free-solid-svg-icons';
-import { faUser } from '@fortawesome/free-solid-svg-icons';
-import { faSearch } from '@fortawesome/free-solid-svg-icons';
-import { faHeart } from '@fortawesome/free-solid-svg-icons';
-import { faBullhorn } from '@fortawesome/free-solid-svg-icons';
-import { faBirthdayCake } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-library.add(faGamepad);
-library.add(faMoneyBillAlt);
-library.add(faPhoneVolume);
-library.add(faGift);
-library.add(faUser);
-library.add(faSearch);
-library.add(faHeart);
-library.add(faBullhorn);
-library.add(faBirthdayCake);
-Vue.component('font-awesome-icon', FontAwesomeIcon);
+import './plugins/fortAwesome.js';
 
 new Vue({
   router,
--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,3 @@
-//const vuxLoader = require('vux-loader');
-
 module.exports = {
-  // configureWebpack: config => {
-  //   vuxLoader.merge(config, {
-  //     plugins: ['vux-ui', 'duplicate-style']
-  //   });
-  // },
   lintOnSave: process.env.NODE_ENV !== 'production'
 };
