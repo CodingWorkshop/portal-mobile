039f4d456d4b614645214f917380f4c5ac1541ba
Mephy912 <qwaszx8411@gmail.com>
2018-11-21 09:53:28 +0800

    Add vue-cli-plugin-i18n package

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,12 @@
 module.exports = {
-  lintOnSave: process.env.NODE_ENV !== 'production'
+  lintOnSave: process.env.NODE_ENV !== 'production',
+
+  pluginOptions: {
+    i18n: {
+      locale: 'en',
+      fallbackLocale: 'en',
+      localeDir: 'locales',
+      enableInSFC: false
+    }
+  }
 };
