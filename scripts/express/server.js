9244bc1baf217cd49eacd45e25344d997d125243
wilson <l7960261@gmail.com>
2018-11-26 16:38:43 +0800

    modify npm start

--- scripts/express/server.js
+++ scripts/express/server.js
@@ -4,6 +4,6 @@ const serveStatic = require('serve-static');
 const path = require('path');
 app = express();
 app.use(history());
-app.use(serveStatic(path.join(__dirname, 'dist')));
+app.use(serveStatic(path.join(__dirname, '..', '..', 'dist')));
 const port = process.env.PORT || 80;
 app.listen(port);
