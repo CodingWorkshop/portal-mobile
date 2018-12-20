cc34c64400b5175461fe4647bb22e5db1dc32ee7
wan-zhen <kelly811124811124@hotmail.com>
2018-12-20 11:46:37 +0800

    use all fontAwesome icon

--- src/plugins/fortAwesome-regular.js
+++ src/plugins/fortAwesome-regular.js
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faHeart } from '@fortawesome/free-regular-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
-library.add(faHeart);
+library.add(far);
 Vue.component('font-awesome-icon', FontAwesomeIcon);
--- src/plugins/fortAwesome-solid.js
+++ src/plugins/fortAwesome-solid.js
@@ -1,31 +1,7 @@
 import Vue from 'vue';
 import { library } from '@fortawesome/fontawesome-svg-core';
-import {
-  faGamepad,
-  faMoneyBillAlt,
-  faPhoneVolume,
-  faGift,
-  faUser,
-  faSearch,
-  faHeart,
-  faBullhorn,
-  faBirthdayCake,
-  faAngleLeft,
-  faSpinner,
-  faExclamationTriangle
-} from '@fortawesome/free-solid-svg-icons';
+import { fas } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
-library.add(faAngleLeft);
-library.add(faBirthdayCake);
-library.add(faBullhorn);
-library.add(faGamepad);
-library.add(faGift);
-library.add(faHeart);
-library.add(faMoneyBillAlt);
-library.add(faPhoneVolume);
-library.add(faSearch);
-library.add(faUser);
-library.add(faSpinner);
-library.add(faExclamationTriangle);
+library.add(fas);
 Vue.component('font-awesome-icon', FontAwesomeIcon);
