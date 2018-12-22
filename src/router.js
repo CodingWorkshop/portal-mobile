ee8cf4b50c4a500f1a93fa844ebfcc4c77890b95
cyan92128505 <cyan92128505@gmail.com>
2018-12-22 17:28:45 +0800

    Modify router.js that remove setupAxios

--- src/router.js
+++ src/router.js
@@ -59,54 +59,50 @@ export default (store, http) => {
     }
   });
 
-  setupAxios(http);
-
-  function setupAxios(http) {
-    http.interceptors.response.use(
-      response => {
-        if (response.data.IsSuccess) {
-          return response.data;
-        } else {
-          return Promise.reject(response.data.ErrorMessage);
-        }
-      },
-      error => {
-        const errorList = [
-          {
-            status: 401,
-            msg: 'Unauthorized',
-            callback: function() {
-              alert('閒置過久，請重新登入');
-              router.push('notfound');
-            }
-          },
-          {
-            status: 404,
-            msg: 'Not Found',
-            callback: function() {
-              //alert('找不到啦');
-            }
-          },
-          {
-            status: 500,
-            msg: 'Internal Server Error',
-            callback: function() {
-              alert('操作頻繁，請稍後嘗試');
-            }
-          }
-        ];
-
-        const currentError = errorList.filter(err => {
-          return err.status === error.response.status;
-        })[0] || {
-          msg: error.response.statusText,
-          callback: () => {}
-        };
-        currentError.callback();
-        return Promise.reject(currentError.msg);
+  http.interceptors.response.use(
+    response => {
+      if (response.data.IsSuccess) {
+        return response.data;
+      } else {
+        return Promise.reject(response.data.ErrorMessage);
       }
-    );
-  }
+    },
+    error => {
+      const errorList = [
+        {
+          status: 401,
+          msg: 'Unauthorized',
+          callback: function() {
+            alert('閒置過久，請重新登入');
+            router.push('notfound');
+          }
+        },
+        {
+          status: 404,
+          msg: 'Not Found',
+          callback: function() {
+            //alert('找不到啦');
+          }
+        },
+        {
+          status: 500,
+          msg: 'Internal Server Error',
+          callback: function() {
+            alert('操作頻繁，請稍後嘗試');
+          }
+        }
+      ];
+
+      const currentError = errorList.filter(err => {
+        return err.status === error.response.status;
+      })[0] || {
+        msg: error.response.statusText,
+        callback: () => {}
+      };
+      currentError.callback();
+      return Promise.reject(currentError.msg);
+    }
+  );
 
   return router;
 };
