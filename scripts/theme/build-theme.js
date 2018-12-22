0e554fef7c6c497516c6de04548801b74c07a838
Smile <cxz0917001997@gmail.com>
2018-12-22 16:21:40 +0800

    Build Theme 腳本 - 產出theme與env後 Run Build

--- scripts/theme/build-theme.js
+++ scripts/theme/build-theme.js
@@ -3,18 +3,26 @@ const path = require('path');
 const axios = require('axios');
 const prettier = require("prettier");
 const dayjs = require('dayjs');
+const { spawn } = require('child_process');
 
 const webSiteCode =  (!process.argv[2]) ? 'staging': process.argv[2];
 const EnvVariableHttpSource = `https://raw.githubusercontent.com/CodingWorkshop/env-portal-mobile/master/.env.${webSiteCode}`;
 
 buildTheme()
-  .then(() => produceWebSiteEnvVariable());
+  .then(() => produceWebSiteEnvVariable())
+  .then(() => RunBuild())
+  .catch(() => console.log(`[${getNow()}][ERROR]:Produce WebSite EnvVariable Error.`));
 
 function produceWebSiteEnvVariable(){
-  axios.get(EnvVariableHttpSource)
+  return axios.get(EnvVariableHttpSource)
   .then(res => generateEnvVariable(res.data))
-  .then(() => console.log('Produce webSite EnvVariable Done.'))
-  .catch(() => console.log('Produce webSite EnvVariable Error.'));
+  .then(() => console.log(`[${getNow()}][INFO]:Produce WebSite EnvVariable Done.`));
+}
+
+function RunBuild(){
+  spawn(/^win/.test(process.platform) ? 'vue-cli-service.cmd' : 'vue-cli-service', ['build',  '--mode', webSiteCode],{
+    stdio: 'inherit'
+  });
 }
 
 function buildTheme() {
@@ -24,6 +32,7 @@ function buildTheme() {
     .catch(err => console.log(
       `[${getNow()}][ERROR]:Build Fail : write file fail ! ${err}`));
 }
+
 
 function prepareThemeHttpSource() {
   const DEFAULT_THEME_SORUCE =
