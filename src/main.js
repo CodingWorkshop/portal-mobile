2e8e2dcadfb5f61cd82076697e6615614e23480d
cyan92128505 <cyan92128505@gmail.com>
2018-12-22 16:18:40 +0800

    move router that from mian.js to router.js

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
 ﻿import Vue from 'vue';
-import axios from 'axios';
-import VueAxios from 'vue-axios';
 import App from './App.vue';
-import router from './router';
+import routerGenerator from './router';
 import store from './store/index';
 import VueTouch from 'vue-touch';
+import axios from 'axios';
+
+const router = routerGenerator(store);
 
 //自定義手勢(連續點擊兩下)
 VueTouch.registerCustomEvent('doubletap', {
@@ -13,7 +14,6 @@ VueTouch.registerCustomEvent('doubletap', {
 });
 
 Vue.config.productionTip = false;
-Vue.use(VueAxios, axios);
 Vue.use(VueTouch, {
   name: 'v-touch'
 });
@@ -24,63 +24,6 @@ import './plugins/iview.js';
 import './plugins/fortAwesome-regular.js';
 import './plugins/fortAwesome-solid.js';
 import i18n from './i18n';
-
-router.beforeEach(function(to, from, next) {
-  const requiredLogin = to.meta.requiredLogin || false;
-  if (requiredLogin && !store.state.login.loginStatus) {
-    store.commit('openDrawerPage', {
-      type: 'login'
-    });
-    store.commit('recordDestinationPage', to.path);
-  } else {
-    next();
-  }
-});
-
-axios.interceptors.response.use(
-  function(response) {
-    if (response.data.IsSuccess) {
-      return response.data;
-    } else {
-      return Promise.reject(response.data.ErrorMessage);
-    }
-  },
-  function(error) {
-    const errorList = [
-      {
-        status: 401,
-        msg: 'Unauthorized',
-        callback: function() {
-          alert('閒置過久，請重新登入');
-          router.push('notfound');
-        }
-      },
-      {
-        status: 404,
-        msg: 'Not Found',
-        callback: function() {
-          //alert('找不到啦');
-        }
-      },
-      {
-        status: 500,
-        msg: 'Internal Server Error',
-        callback: function() {
-          alert('操作頻繁，請稍後嘗試');
-        }
-      }
-    ];
-
-    const currentError = errorList.filter(err => {
-      return err.status === error.response.status;
-    })[0] || {
-      msg: error.response.statusText,
-      callback: function() {}
-    };
-    currentError.callback();
-    return Promise.reject(currentError.msg);
-  }
-);
 
 function loadLangs() {
   axios.get('https://next.json-generator.com/api/json/get/VJWKeCGlU').then(
--- src/router.js
+++ src/router.js
@@ -1,45 +1,115 @@
 import Vue from 'vue';
 import Router from 'vue-router';
+import axios from 'axios';
+import VueAxios from 'vue-axios';
+
 import Home from './views/Home.vue';
 import Account from './views/Account.vue';
 import NotFound from './views/NotFound.vue';
-Vue.use(Router);
 
-export default new Router({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home
-    },
-    {
-      path: '/wallet',
-      name: 'wallet',
-      component: () => import('./views/Wallet.vue')
-    },
-    {
-      path: '/promotion',
-      name: 'promotion',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () =>
-        import(/* webpackChunkName: "about" */ './views/Promotion.vue')
-    },
-    {
-      path: '/account',
-      name: 'account',
-      component: Account,
-      meta: {
-        //requiredLogin: true
+export default store => {
+  Vue.use(VueAxios, axios);
+  Vue.use(Router);
+  const router = new Router({
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes: [
+      {
+        path: '/',
+        name: 'home',
+        component: Home
+      },
+      {
+        path: '/wallet',
+        name: 'wallet',
+        component: () => import('./views/Wallet.vue')
+      },
+      {
+        path: '/promotion',
+        name: 'promotion',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () =>
+          import(/* webpackChunkName: "about" */
+          './views/Promotion.vue')
+      },
+      {
+        path: '/account',
+        name: 'account',
+        component: Account,
+        meta: {
+          //requiredLogin: true
+        }
+      },
+      {
+        path: '*',
+        name: 'notfound',
+        component: NotFound
       }
-    },
-    {
-      path: '*',
-      name: 'notfound',
-      component: NotFound
-    }
-  ]
-});
+    ]
+  });
+
+  setupAxios(store);
+
+  function setupAxios(store) {
+    router.beforeEach(function(to, from, next) {
+      const requiredLogin = to.meta.requiredLogin || false;
+      if (requiredLogin && !store.state.login.loginStatus) {
+        store.commit('openDrawerPage', {
+          type: 'login'
+        });
+        store.commit('recordDestinationPage', to.path);
+      } else {
+        next();
+      }
+    });
+
+    axios.interceptors.response.use(
+      response => {
+        if (response.data.IsSuccess) {
+          return response.data;
+        } else {
+          return Promise.reject(response.data.ErrorMessage);
+        }
+      },
+      error => {
+        const errorList = [
+          {
+            status: 401,
+            msg: 'Unauthorized',
+            callback: function() {
+              alert('閒置過久，請重新登入');
+              router.push('notfound');
+            }
+          },
+          {
+            status: 404,
+            msg: 'Not Found',
+            callback: function() {
+              //alert('找不到啦');
+            }
+          },
+          {
+            status: 500,
+            msg: 'Internal Server Error',
+            callback: function() {
+              alert('操作頻繁，請稍後嘗試');
+            }
+          }
+        ];
+
+        const currentError = errorList.filter(err => {
+          return err.status === error.response.status;
+        })[0] || {
+          msg: error.response.statusText,
+          callback: () => {}
+        };
+        currentError.callback();
+        return Promise.reject(currentError.msg);
+      }
+    );
+  }
+
+  return router;
+};
