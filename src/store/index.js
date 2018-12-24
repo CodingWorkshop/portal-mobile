1b578b824b834870e19bb5633d89f29f036d0e55
Mephy912 <qwaszx8411@gmail.com>
2018-12-24 10:16:21 +0800

    add i18n module to store

--- src/store/index.js
+++ src/store/index.js
@@ -4,26 +4,24 @@ import createPersistedState from 'vuex-persistedstate';
 
 import login from './modules/login';
 import drawer from './modules/drawer';
+import i18n from './modules/i18n';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   modules: {
     login: login,
-    drawer: drawer
+    drawer: drawer,
+    i18n: i18n
   },
   plugins: [
     createPersistedState({
       storage: {
         getItem: key => localStorage.getItem(key),
-        setItem: (key, value) =>
-          localStorage.setItem(key, value, {
-            expires: 3,
-            secure: true
-          }),
+        setItem: (key, value) => localStorage.setItem(key, value),
         removeItem: key => localStorage.removeItem(key)
       },
-      paths: ['login.loginStatus', 'login.user']
+      paths: ['login.loginStatus', 'login.user', 'i18n.version', 'i18n.message']
     })
   ]
 });
