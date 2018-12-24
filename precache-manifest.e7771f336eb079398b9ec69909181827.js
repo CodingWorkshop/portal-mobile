56c0fd22d1fc82c7ac16603ae26711fc50db1a3b
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-12-24 08:25:38 +0000

    Deploy CodingWorkshop/portal-mobile to github.com/CodingWorkshop/portal-mobile.git:gh-pages

---
+++ precache-manifest.e7771f336eb079398b9ec69909181827.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a2fb3d2ca49041f6414",
+    "url": "/portal-mobile/css/about.6cc656a7.css"
+  },
+  {
+    "revision": "4a2fb3d2ca49041f6414",
+    "url": "/portal-mobile/js/about.bdec99be.js"
+  },
+  {
+    "revision": "a212d1d33b997239db0f",
+    "url": "/portal-mobile/css/app.2f671a6f.css"
+  },
+  {
+    "revision": "a212d1d33b997239db0f",
+    "url": "/portal-mobile/js/app.f7db9980.js"
+  },
+  {
+    "revision": "5fed9b4903e2564b5077",
+    "url": "/portal-mobile/css/chunk-176bc395.49d7a741.css"
+  },
+  {
+    "revision": "5fed9b4903e2564b5077",
+    "url": "/portal-mobile/js/chunk-176bc395.3026f36b.js"
+  },
+  {
+    "revision": "474204a5050c75313d7e",
+    "url": "/portal-mobile/js/chunk-vendors.6c3de38e.js"
+  },
+  {
+    "revision": "72336af233acc8b76d60f3cc529da0b6",
+    "url": "/portal-mobile/img/cake.72336af2.png"
+  },
+  {
+    "revision": "d535a25a79fb1365ae814b61e88fae71",
+    "url": "/portal-mobile/fonts/ionicons.d535a25a.ttf"
+  },
+  {
+    "revision": "99ac3308dd8ee14f749f51538d0d5b9e",
+    "url": "/portal-mobile/fonts/ionicons.99ac3308.woff"
+  },
+  {
+    "revision": "a2c4a261a239aa84463dc70e4bac9b9a",
+    "url": "/portal-mobile/img/ionicons.a2c4a261.svg"
+  },
+  {
+    "revision": "631d23b7877da2a4e90f20d610725fc6",
+    "url": "/portal-mobile/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/portal-mobile/robots.txt"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portal-mobile/precache-manifest.17a032fad041f7344446880eb8848299.js"
+  "/portal-mobile/precache-manifest.e7771f336eb079398b9ec69909181827.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "portal-mobile"});
