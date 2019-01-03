7d62423ac66c0fdc07fc889020e6eb0031f418c5
cyan92128505 <cyan92128505@gmail.com>
2019-01-03 13:40:05 +0800

    Modify Import Touch Method

--- src/main.ts
+++ src/main.ts
@@ -3,17 +3,15 @@ import App from './App.vue';
 import http from './http';
 import store from './store/index';
 import routerGenerator from './router';
-import prepareTouch from './touch';
-
-Vue.config.productionTip = false;
-
-const router = routerGenerator(store, http);
-prepareTouch();
-
+import './touch';
 import './plugins/iview.ts';
 import './plugins/fortAwesome-regular.ts';
 import './plugins/fortAwesome-solid.ts';
 import { default as i18n, loadLangs } from './i18n';
+
+Vue.config.productionTip = false;
+
+const router = routerGenerator(store, http);
 
 new Vue({
   router,
--- src/touch.ts
+++ src/touch.ts
@@ -1,18 +1,16 @@
 import Vue from 'vue';
 const VueTouch = require('vue-touch');
 
-export default () => {
-  //自定義手勢(連續點擊兩下)
-  VueTouch.registerCustomEvent('doubletap', {
-    type: 'tap',
-    taps: 2
-  });
+//自定義手勢(連續點擊兩下)
+VueTouch.registerCustomEvent('doubletap', {
+  type: 'tap',
+  taps: 2
+});
 
-  Vue.config.productionTip = false;
-  Vue.use(VueTouch, {
-    name: 'v-touch'
-  });
-  VueTouch.config.swipe = {
-    direction: 'horizontal'
-  };
+Vue.config.productionTip = false;
+Vue.use(VueTouch, {
+  name: 'v-touch'
+});
+VueTouch.config.swipe = {
+  direction: 'horizontal'
 };
