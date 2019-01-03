b173beeab6680ff9eb89a64580a93c0a8ff3640d
Eliza <boy877817121@gmail.com>
2019-01-03 17:41:31 +0800

    註冊畫面完成50%

---
+++ src/components/common/FormInput.vue
@@ -0,0 +1,42 @@
+<script>
+export default {
+  name: 'FormInput',
+  props: {
+    detail: Object,
+    modelForm: Object
+  },
+  render: function(createElement) {
+    return createElement(
+      'FormItem',
+      {
+        props: {
+          prop: this.detail.model,
+          //rules:{required: true, message: 'can not be empty', trigger: 'blur'}
+          required: this.detail.isRequired
+        }
+      },
+      [
+        createElement('i-input', {
+          props: {
+            //pattern: this.detail.pattern ? this.detail.pattern : '',
+          },
+          attrs: {
+            placeholder: this.detail.placeholder,
+            maxlength: this.detail.maxlength ? this.detail.maxlength : null,
+            type: this.detail.type,
+            clearable: true,
+            value: this.modelForm[this.detail.model]
+          }
+        })
+      ]
+    );
+  },
+  data: function() {
+    return {};
+  },
+  methods: {},
+  mounted: function() {}
+};
+</script>
+<style lang="less" scoped>
+</style>
--- src/views/drawer/Login.vue
+++ src/views/drawer/Login.vue
@@ -33,42 +33,8 @@
       </tab-pane>
       <tab-pane label="立即註冊" name="register">
         <v-touch tag="body" v-on:swiperight="key='login'">
-          <Form @submit.native.prevent="registerSubmit" ref="registerForm"
-            :model="registerForm" :rules="registerRuleValidate">
-            <FormItem prop="Referrer">
-              <i-input v-model="registerForm.referrer" placeholder="推薦人"
-                clearable>
-              </i-input>
-            </FormItem>
-            <FormItem prop="user">
-              <i-input v-model="registerForm.user" placeholder="會員帳號 需字母開頭 限數字和底線"
-                clearable>
-              </i-input>
-            </FormItem>
-            <FormItem prop="password">
-              <i-input v-model="registerForm.password" type="password"
-                placeholder="會員密碼" clearable>
-              </i-input>
-            </FormItem>
-            <FormItem prop="checkPsd">
-              <i-input v-model="registerForm.checkPsd" type="password"
-                placeholder="確認密碼" clearable>
-              </i-input>
-            </FormItem>
-            <FormItem prop="withdrawalPsd">
-              <i-input v-model="registerForm.withdrawalPsd" placeholder="取款密碼"
-                clearable>
-              </i-input>
-            </FormItem>
-            <FormItem prop="realName">
-              <i-input v-model="registerForm.realName" placeholder="真實姓名 需與提款銀行戶口相同"
-                clearable>
-              </i-input>
-            </FormItem>
-            <FormItem prop="code">
-              <i-input v-model="registerForm.code" placeholder="驗證碼" clearable>
-              </i-input>
-            </FormItem>
+          <Form @submit.native.prevent="registerSubmit('registerForm')" :model="registerForm" ref="registerForm">
+            <form-input v-for="(item, index) in registerList" :key="index" :detail="item" :modelForm="registerForm"></form-input>
             <FormItem>
               <Button html-type="submit" type="primary" long>送出</Button>
             </FormItem>
@@ -84,83 +50,98 @@
 </template>
 
 <script>
+import FormInput from '@/components/common/FormInput.vue';
+const registerList = [
+  {
+    name: '推薦人',
+    model: 'Referrer',
+    placeholder: '請輸入推薦人',
+    type: 'text'
+  },
+  {
+    name: '會員帳號',
+    model: 'Account',
+    placeholder: '請輸入會員帳號 需字母開頭 限數字和底線',
+    type: 'text',
+    maxlength: 15,
+    pattern: '/^[a-zA-Z]|d+/'
+  },
+  {
+    name: '會員密碼',
+    model: 'Password',
+    placeholder: '請輸入會員密碼',
+    type: 'password',
+    pattern: '/[a-zA-Z]|d+/'
+  },
+  {
+    name: '確認密碼',
+    model: 'CheckPsd',
+    placeholder: '請再確認密碼',
+    type: 'password'
+  },
+  {
+    name: '取款密碼',
+    model: 'MoneyPassword',
+    placeholder: '請輸入取款密碼',
+    type: 'password'
+  },
+  {
+    name: '真實姓名',
+    model: 'Name',
+    placeholder: '請輸入真實姓名 需與提款銀行戶口相同',
+    type: 'text'
+  },
+  {
+    name: '手機號碼',
+    model: 'Mobile',
+    placeholder: '請輸入手機號碼',
+    type: 'text',
+    pattern: '/[0-9]/'
+  },
+  {
+    name: '電子信箱',
+    model: 'Email',
+    placeholder: '請輸入電子信箱',
+    type: 'email'
+  },
+  {
+    name: '性別',
+    model: 'Sex',
+    placeholder: '請輸入性別',
+    type: 'text'
+  },
+  {
+    name: '生日',
+    model: 'Birthday',
+    placeholder: '請輸入生日',
+    type: 'date'
+  },
+  {
+    name: 'QQ',
+    model: 'QQ',
+    placeholder: '請輸入QQ',
+    type: 'text'
+  },
+  {
+    name: '微信',
+    model: 'IdNumber',
+    placeholder: '請輸入微信',
+    type: 'text'
+  }
+];
 export default {
   name: 'DrawerLogin',
-  components: {},
+  components: {
+    FormInput
+  },
   data: function() {
     return {
       key: 'login',
+      masterRule: [],
       loginForm: {
         user: '',
         password: '',
         code: ''
-      },
-      registerForm: {
-        referrer: '',
-        user: '',
-        password: '',
-        checkPsd: '',
-        withdrawalPsd: '',
-        realName: '',
-        code: ''
-      },
-      registerRuleValidate: {
-        referrer: [
-          {
-            required: true,
-            type: 'string',
-            message: 'The referrer cannot be empty',
-            trigger: 'blur'
-          }
-        ],
-        user: [
-          {
-            required: true,
-            type: 'string',
-            message: 'The user cannot be empty',
-            trigger: 'blur'
-          }
-        ],
-        password: [
-          {
-            required: true,
-            type: 'string',
-            message: 'The password cannot be empty',
-            trigger: 'blur'
-          }
-        ],
-        checkPsd: [
-          {
-            required: true,
-            type: 'string',
-            message: 'The check password cannot be empty',
-            trigger: 'blur'
-          }
-        ],
-        withdrawalPsd: [
-          {
-            required: true,
-            type: 'integer',
-            message: 'The withdrawal password name cannot be empty',
-            trigger: 'blur'
-          }
-        ],
-        realName: [
-          {
-            required: true,
-            type: 'string',
-            message: 'The real name cannot be empty',
-            trigger: 'blur'
-          }
-        ],
-        code: [
-          {
-            required: true,
-            type: 'integer',
-            message: 'The code cannot be empty',
-            trigger: 'blur'
-          }
-        ]
       },
       loginRuleValidate: {
         user: [
@@ -185,50 +166,76 @@ export default {
             trigger: 'blur'
           }
         ]
-      }
+      },
+      registerForm: {
+        Referrer: '',
+        Account: '',
+        Password: '',
+        CheckPsd: '',
+        MoneyPassword: '',
+        Name: '',
+        Mobile: '',
+        Email: '',
+        Sex: '',
+        Birthday: '',
+        QQ: '',
+        IdNumber: ''
+      },
+      registerList: []
     };
   },
   methods: {
     loginSubmit: function(name) {
       this.$store.commit('updateSigning');
+
       this.$refs[name].validate(valid => {
-        if (valid) {
-          this.axios
-            .post(process.env.VUE_APP_LOGIN_URL, {
-              Account: this.loginForm.user,
-              Password: this.loginForm.password
-            })
-            .then(
-              response => {
-                const token = response.ReturnObject;
-
-                this.$store.dispatch('updateLogin', {
-                  user: this.loginForm.user,
-                  token: token
-                });
-
-                this.$Message.success('登入成功!');
-
-                const destinationPage = this.$store.state.login.destinationPage;
-                if (destinationPage) {
-                  this.$router.push(destinationPage);
-                  this.$store.commit('recordDestinationPage', '');
-                  this.$store.commit('closeDrawerPage', 'login');
-                }
-
-                this.$store.commit('updateSigning');
-              },
-              () => {
-                this.$Message.error('帳號密碼錯誤!');
-                this.$store.commit('updateSigning');
-              }
-            );
-        } else {
+        if (!valid) {
           this.$store.commit('updateSigning');
+          return;
         }
+
+        this.axios
+          .post(process.env.VUE_APP_LOGIN_URL, {
+            Account: this.loginForm.user,
+            Password: this.loginForm.password
+          })
+          .then(
+            response => {
+              const token = response.ReturnObject;
+
+              this.$store.dispatch('updateLogin', {
+                user: this.loginForm.user,
+                token: token
+              });
+
+              this.$Message.success('登入成功!');
+
+              const destinationPage = this.$store.state.login.destinationPage;
+              if (destinationPage) {
+                this.$router.push(destinationPage);
+                this.$store.commit('recordDestinationPage', '');
+                this.$store.commit('closeDrawerPage', 'login');
+              }
+
+              this.$store.commit('updateSigning');
+            },
+            () => {
+              this.$Message.error('帳號密碼錯誤!');
+              this.$store.commit('updateSigning');
+            }
+          );
       });
     },
-    registerSubmit: function() {},
+    registerSubmit: function(name) {
+      this.$refs[name].validate(valid => {
+        console.log(this.registerForm);
+        if (!valid) {
+          this.$Message.success('失敗!');
+          return;
+        }
+        this.$Message.success('成功!');
+      });
+    },
     logOut: function() {
       this.$store.dispatch('submitLogout');
       this.loginForm = {
@@ -238,6 +245,24 @@ export default {
       };
       this.$Message.success('登出成功!');
     }
+  },
+  mounted: function() {
+    this.axios
+      .get('https://next.json-generator.com/api/json/get/V1bIvscJU')
+      .then(response => {
+        this.masterRule = response.ReturnObject;
+        registerList.forEach(r => {
+          r.isRequired =
+            this.masterRule['IsRequired_' + r.name] == undefined
+              ? true
+              : this.masterRule['IsRequired_' + r.name];
+          r.isShow =
+            this.masterRule['IsShow_' + r.name] == undefined
+              ? true
+              : this.masterRule['IsShow_' + r.name];
+        });
+        this.registerList = registerList.filter(r => r.isShow == true);
+      });
   }
 };
 </script>
