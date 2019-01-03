c280b64a0b22bea331a03126ba1cd1852767d174
cyan92128505 <cyan92128505@gmail.com>
2019-01-03 10:03:00 +0800

    Modifty main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,26 @@
-import './main-old';
+import Vue from 'vue';
+import App from './App.vue';
+import http from './http';
+import store from './store/index';
+import routerGenerator from './router';
+import prepareTouch from './touch';
+
+Vue.config.productionTip = false;
+
+const router = routerGenerator(store, http);
+prepareTouch();
+
+import './plugins/iview.js';
+import './plugins/fortAwesome-regular.js';
+import './plugins/fortAwesome-solid.js';
+import { default as i18n, loadLangs } from './i18n';
+
+new Vue({
+  router,
+  store,
+  i18n,
+  created: function() {
+    loadLangs(store, http);
+  },
+  render: h => h(App)
+}).$mount('#app');
