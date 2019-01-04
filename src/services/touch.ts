3f60a5312180a73c52befdd055023c571c511121
Smile <cxz0917001997@gmail.com>
2019-01-04 10:28:40 +0800

    add  vue-plugin-touch

--- src/services/touch.ts
+++ src/services/touch.ts
@@ -1,4 +1,5 @@
 import Vue from 'vue';
+const vuePluginTouch = require('vue-plugin-touch');
 const VueTouch = require('vue-touch');
 
 //自定義手勢(連續點擊兩下)
@@ -11,6 +12,7 @@ Vue.config.productionTip = false;
 Vue.use(VueTouch, {
   name: 'v-touch'
 });
+Vue.use(vuePluginTouch);
 VueTouch.config.swipe = {
   direction: 'horizontal'
 };
