d9bd4ded433796315864ea28c9fb0d4a3a87f59c
Smile <cxz0917001997@gmail.com>
2019-01-07 15:46:36 +0800

    Change WalletComponet apply SlideOptimizing setting

--- src/views/Wallet.vue
+++ src/views/Wallet.vue
@@ -1,25 +1,24 @@
 <template>
   <div style="height:100%">
     <h1>帳務中心</h1>
-    <slide-optimizing v-bind:pageLength="walletDetail.length">
-      <tabs v-model="key" size="small">
+    <slide-optimizing
+      :pageList="walletDetail"
+      :pageIndex="pageIndex"
+      :activeKey="activeKey"
+      :setActiveKey="setActiveKey"
+    >
+      <tabs size="small" v-model="activeKey" @on-click="setActiveKey($event)">
         <tab-pane
           v-for="(i,index) in walletDetail"
           :key="index"
           v-bind:label="i.label"
           v-bind:name="i.name"
         >
-          <!--<v-touch
-            tag="body"
-            v-on:swipeleft="activeKey(index+1,i.name)"
-            v-on:swiperight="activeKey(index-1,i.name)"
-          >-->
-            <div>
-              <keep-alive>
-                <component :is="i.name"></component>
-              </keep-alive>
-            </div>
-          <!--</v-touch>-->
+          <div>
+            <keep-alive>
+              <component :is="i.name"></component>
+            </keep-alive>
+          </div>
         </tab-pane>
       </tabs>
     </slide-optimizing>
@@ -42,14 +41,24 @@ export default {
     SlideOptimizing
   },
   methods: {
-    activeKey: function(count, val) {
-      this.key = count >= 0 && count < 4 ? this.walletDetail[count].name : val;
+    /*setActiveKey: function(count, val) {
+      this.activeKey = count >= 0 && count < 4 ? this.walletDetail[count].name : val;
+      this.pageIndex = count;
+      console.log('key: ', this.activeKey);
+    },*/
+    setActiveKey(itemName) {
+      var index = this.walletDetail.findIndex(
+        item => item.name === itemName.name
+      );
+      this.pageIndex = index;
+      this.activeKey = itemName.name;
     }
   },
   data: function() {
     return {
-      key: '',
-      walletDetail: []
+      pageIndex: 0,
+      activeKey: '',
+      walletDetail: ['Deposit', 'Withdrawal', 'Discount', 'Transaction']
     };
   },
   mounted: function() {
